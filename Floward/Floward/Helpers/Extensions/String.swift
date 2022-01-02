28518a264dd303fd68dee229115ed52807b8be63
Adham Alkhateeb <adhamykh@gmail.com>
2022-01-02 15:33:25 +0400

    Create environment configuration

---
+++ Floward/Floward/Helpers/Extensions/String.swift
@@ -0,0 +1,12 @@
+/*
+ *	String.swift
+ *	LevelShoes
+ *
+ *	Created by Klindayzer on 21/12/2021.
+ *	Copyright 2021 ___ORGANIZATIONNAME___. All rights reserved.
+ */
+
+extension String {
+    
+    static var empty: String = ""
+}
